145736b9b86cc126d474a55ef9edcaf733f3b5a9
Amit Tiwari <smilyamitt@gmail.com>
2020-05-06 19:16:14 +0200

    all

---
+++ src/@lekoarts/gatsby-theme-cara/components/footer.tsx
@@ -0,0 +1,43 @@
+/** @jsx jsx */
+import { Box, Flex, Link, useColorMode, jsx } from "theme-ui"
+
+const Footer = () => {
+  const [colorMode, setColorMode] = useColorMode()
+  const isDark = colorMode === `dark`
+  const toggleColorMode = (e: any) => {
+    setColorMode(isDark ? `light` : `dark`)
+  }
+
+  return (
+    <Box as="footer" variant="footer">
+      <button
+        sx={{ variant: `buttons.toggle`, fontWeight: `semibold`, display: `block`, mx: `auto`, mb: 3 }}
+        onClick={toggleColorMode}
+        type="button"
+        aria-label="Toggle dark mode"
+      >
+        {isDark ? `Light` : `Dark`}
+      </button>
+       ©AmitTiwari {new Date().getFullYear()}
+      <br />
+      <h4>+491782571880</h4>
+      <Flex
+        sx={{
+          justifyContent: `center`,
+          alignItems: `center`,
+          mt: 3,
+          color: `text`,
+          fontWeight: `semibold`,
+          a: { color: `text` },
+        }}
+      >
+        <h4>Theme</h4>
+        <div sx={{ mx: 1 }}>by</div>
+        {` `}
+        <h4>LekoArts</h4>
+      </Flex>
+    </Box>
+  )
+}
+
+export default Footer
