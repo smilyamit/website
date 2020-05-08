e01bb3461fc7e782a2e2de6aeeacce298282f97d
Amit Tiwari <smilyamitt@gmail.com>
2020-05-08 19:54:10 +0200

    config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,6 @@ require(`dotenv`).config({
 })
 
 module.exports = {
-  pathPrefix: "/website",
   siteMetadata: {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
